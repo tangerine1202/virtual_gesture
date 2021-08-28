9a08652e0ce9e7315ae861632fc764faca998b7e
Alan Huang <alan0988469026@gmail.com>
2021-08-29 02:14:41 +0800

    split draw_fn to another file & update filter params

---
+++ draw_utils.py
@@ -0,0 +1,82 @@
+from utils import dotdict
+
+import cv2
+import numpy as np
+
+COLOR = dotdict({
+  'black': (0,0,0),
+  'gray':	(128,128,128),
+  'silver':	(192,192,192),
+  'white': (255,255,255),
+  'red': (255,0,0),
+  'yellow':	(255,255,0),
+  'lime':	(0,255,0),
+  'cyan':	(0,255,255),
+  'blue':	(0,0,255),
+  'magenta': (255,0,255),
+})
+
+def draw_handedness(img, handedness):
+    if handedness[0] < .5:
+        label = 'Left'
+    elif handedness[0] > .5:
+        label = 'Right'
+    else:
+        label = 'Unknown'
+
+    img_w, img_h = img.shape[1], img.shape[0]
+    cv2.putText(img, f'{label} {np.abs((handedness[0]-.5)*2) :.2f}',
+                org=(img_w//2 - 200, 30),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=1,
+                color=COLOR.red,
+                lineType=2)
+
+
+def draw_landmarks(img, landmarks, color=(50, 255, 50)):
+
+    img_w, img_h = img.shape[1], img.shape[0]
+
+    # draw dots
+    for i in range(21):
+        p = landmarks[i, 0:2, 0]
+        cv2.circle(img, p.astype(int), radius=4,
+                   thickness=-1, color=COLOR.red)
+
+    # draw connection
+    pairs = []
+    # wrist to index~pinky
+    pairs.append((0, 1, COLOR.silver))
+    pairs.append((0, 5, COLOR.silver))
+    pairs.append((0, 17, COLOR.silver))
+    # finger mcp to finger mcp
+    pairs.append((5, 9, COLOR.silver))
+    pairs.append((9, 13, COLOR.silver))
+    pairs.append((13, 17, COLOR.silver))
+    # mcp to tip
+    for i, c in zip(range(1, 21, 4), [COLOR.magenta, COLOR.lime, COLOR.cyan, COLOR.blue, COLOR.yellow]):
+        for j in range(3):
+            pairs.append((i+j, i+j+1, c))
+
+    for pp1, pp2, c in pairs:
+        p1 = landmarks[pp1, 0:2, 0]
+        p2 = landmarks[pp2, 0:2, 0]
+        cv2.line(img, p1.astype(int), p2.astype(int), thickness=2, color=c)
+
+
+def draw_finger_state(img, handedness, finger_states):
+    img_w, img_h = img.shape[1], img.shape[0]
+
+    state_str = ''
+    for finger_idx in range(5):
+        if handedness[0] < 0.5:
+            state_str = f'{finger_states[finger_idx].value}{state_str}'
+        else:
+            state_str = f'{state_str}{finger_states[finger_idx].value}'
+    cv2.putText(img, state_str,
+                org=(img_w//2, 30),  # bottomLeftCornerOfText
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=1,
+                color=COLOR.red,
+                lineType=2)
+
